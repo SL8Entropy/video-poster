63ddc6d1920ee9a19a1cab7b7406ebacbeb2fa54
SL8Entropy <sudharshan.sambathkumar@gmail.com>
2024-08-05 08:43:10 +0530

    FInished prototype
    made it so that i remembers which number it stops at each session of uploading, and it uses 3 google cloud console projects instead of 1 to bypass token limits in each. Yet to test.

--- video-poster-main/posting_to_youtube.py
+++ video-poster-main/posting_to_youtube.py
@@ -18,12 +18,26 @@ API_VERSION = "v3"
 
 
 # Authenticate and authorize
-def get_authenticated_service():
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+def get_authenticated_service(Input):
+    
+    if (Input ==1):
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         "client_secret_83190869396-krraupejd6asno6t6ctok8qtf82st9s5.apps.googleusercontent.com.json", SCOPES)
-    credentials = flow.run_local_server(port=0)
+        credentials = flow.run_local_server(port=0)
+    elif (Input == 2):
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        "client_secret_949978928070-pu7g01jbd3ndco42rdvqjjv3ch2nhsnc.apps.googleusercontent.com.json", SCOPES)
+        credentials = flow.run_local_server(port=0)
+    elif(Input == 3):
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        "client_secret_967439837831-26u1s4qfvlrurioka0ltbkd6kkhsl6gn.apps.googleusercontent.com.json", SCOPES)
+        credentials = flow.run_local_server(port=0)
+    else:
+        print("Finished")
+        
     return googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
+    
 def upload_video(video_file, title, description, tags, publish_time):
     body = {
         "snippet": {
@@ -49,7 +63,7 @@ def upload_video(video_file, title, description, tags, publish_time):
     print(f"Uploaded video with ID: {response['id']}")
 if __name__ == "__main__":
     
-    start_number = 20 #put starting vid number
+    
     
     date_modifier = 4 #put starting day
     month_modifier = 8 #put starting month
@@ -60,23 +74,38 @@ if __name__ == "__main__":
     fileName = "ai_shorts_psychology_with_separate_tags.xlsx"
     path = os.path.join(script_dir, fileName)
     wb = openpyxl.load_workbook(path)
-    sheet = wb.active
+    sheet1 = wb["Sheet1"]
+    sheet2 = wb["Sheet2"]
 
+
+    start_number = int(sheet2.cell(row = 1, column = 1).value)
     
-    youtube = get_authenticated_service()
+    appNo = 3
+    count = 1
+    
+    youtube = get_authenticated_service(appNo)
+    
+    for j in range(1, start_number+1):
+        re = j%3
+        if re == 2:
+            date_modifier += 1
+            if (date_modifier >=30):
+                date_modifier = 1
+                month_modifier += 1
+
     for i in range(start_number,start_number+no_vids):
         I=str(i)
         videoName = "videos to post\\"+I+".mp4"
         video_file = os.path.join(script_dir, videoName)
-        titleTemp = sheet.cell(row = i+1,column = 1)
+        titleTemp = sheet1.cell(row = i+1,column = 1)
         title = str(titleTemp.value)
 
-        descriptionTemp = sheet.cell(row = i+1,column = 2)
+        descriptionTemp = sheet1.cell(row = i+1,column = 2)
         description = str(descriptionTemp.value)
 
         tags = []
         for j in range(1,16):
-            appendable  = sheet.cell(row = i+1, column = j+2)
+            appendable  = sheet1.cell(row = i+1, column = j+2)
             tags.append(str(appendable.value))
 
         
@@ -84,21 +113,31 @@ if __name__ == "__main__":
 
         if (remainder == 0):
             publish_time = datetime(2024, month_modifier, date_modifier, 4, 0, 0).isoformat() + "Z"
-            sleep_amount = 10
         elif(remainder == 1):
             publish_time = datetime(2024, month_modifier, date_modifier, 12, 0, 0).isoformat() + "Z"
-            sleep_amount = 10
         elif(remainder == 2):
             publish_time = datetime(2024, month_modifier, date_modifier, 20, 0, 0).isoformat() + "Z"
             date_modifier += 1
-            sleep_amount = 60
             if (date_modifier >=30):
                 date_modifier = 1
                 month_modifier += 1
         else:
             print("error")
 
-        print(video_file, title, description, tags, publish_time)
 
         upload_video(video_file, title, description, tags, publish_time)
-        time.sleep(sleep_amount)
+        print(video_file, title, description, tags, publish_time)
+        
+        
+        
+        
+        if (count ==7):
+            count = 0
+            appNo+=1
+            youtube = get_authenticated_service(appNo)
+        else:
+            count +=1
+        sheet2['A1'] = i
+        wb.save(path)
+        
+        time.sleep(5)
